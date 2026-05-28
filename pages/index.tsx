3805ecf905411b31d9ece412056e1533959848b2
Salsaal shahid <salsaalshahid9038@gmail.com>
2026-05-28 09:10:50 +0530

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/overview");
+  }, [router]);
+
   return <div></div>;
 }
